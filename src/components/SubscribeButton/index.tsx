8e7b7d0f61fe06a4d8754e846c9abd6260bcdba3
kaiquemaia <kaiquefeng1@gmail.com>
2021-03-25 15:01:56 -0300

    feat: created front-end  application ig.news

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss'
+
+interface SubscribeBurronProps {
+  priceId: string
+}
+
+export function SubscribeButton({ priceId }: SubscribeBurronProps) {
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+    >
+      Subscribe Now
+    </button>
+  )
+}
